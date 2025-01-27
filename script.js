7cafc8a728ad8b9e3774a4cc986ee44b029b2561
Giovanni Delle Lenti <giovanni.dellelenti@gmail.com>
2025-01-27 17:41:00 +0100

    fix header menu

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-
-
-
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-            entry.target.classList.remove('hidden');
-        } else {
-            entry.target.classList.add('hidden');
-            entry.target.classList.remove('show');
+        const target = entry.target;
+        if (entry.isIntersecting && !target.classList.contains('visible')) {
+            // Se l'elemento entra nella viewport ed è nascosto
+            target.classList.add('visible', 'animate-show');
+            target.classList.remove('hidden', 'animate-hide');
+        } else if (!entry.isIntersecting && target.classList.contains('visible')) {
+            // Se l'elemento esce dalla viewport ed è visibile
+            target.classList.add('hidden', 'animate-hide');
+            target.classList.remove('visible', 'animate-show');
         }
     });
 }, { threshold: 0.1 });
@@ -18,5 +17,3 @@ document.addEventListener('DOMContentLoaded', () => {
     const hiddenRows = document.querySelectorAll('.hidden');
     hiddenRows.forEach((row) => observer.observe(row));
 });
-
-
